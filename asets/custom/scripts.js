527c1e4b7d598692d497663c710cf1b341ed6b41
Sagarkalis <lergo2004@gmail.com>
2023-12-01 21:12:57 +0100

    Fixed js and some buttons

--- asets/custom/scripts.js
+++ asets/custom/scripts.js
@@ -1,13 +1,26 @@
 function main() {
-  const btns = document.querySelectorAll(".find-button");
-  btns.forEach((btn) => {
-    btn.addEventListener("click", findVehicle);
-  });
+  const btn_find = document.querySelector(".find-button");
+  const btn_see_rentals = document.querySelector(".button-see-rent");
+  if (btn_find) {
+    btn_find.addEventListener("click", findVehicle);
+  }
+  if (btn_see_rentals) {
+    btn_see_rentals.addEventListener("click", seeRentals);
+  }
 }
 
 function findVehicle() {
-  console.log("Stacili ste find btn meow");
-  // need to send variant od vehicle, location and dates
-  // to back or just save, send leter
-  window.open("./rent_vehicle.html");
+  try {
+    console.log("Find button was pressed");
+    // need to send variant od vehicle, location and dates
+    // to back or just save, send leter
+    window.open("/rent_vehicle.html");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function seeRentals() {
+  console.log("See rentals button was pressed");
+  window.open("/rentals.html");
 }
