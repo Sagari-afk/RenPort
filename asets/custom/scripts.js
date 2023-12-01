1d6bdcc73dceebd70776647add191a8d34e3f075
Sagarkalis <lergo2004@gmail.com>
2023-12-01 18:56:21 +0100

    Started step 1 in rent_vehicle

--- asets/custom/scripts.js
+++ asets/custom/scripts.js
@@ -7,4 +7,7 @@ function main() {
 
 function findVehicle() {
   console.log("Stacili ste find btn meow");
+  // need to send variant od vehicle, location and dates
+  // to back or just save, send leter
+  window.open("./rent_vehicle.html");
 }
